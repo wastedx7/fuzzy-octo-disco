abb681eccf589fd779c38ef9c73bc5e737a49499
wastedx7 <wastedx7@gmail.com>
2026-04-21 19:29:35 +0530

    commit

--- retail/src/main/java/com/retail/controllers/CategoryController.java
+++ retail/src/main/java/com/retail/controllers/CategoryController.java
@@ -1,20 +1,27 @@
 package com.retail.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.retail.io.CategoryRequest;
 import com.retail.io.CategoryResponse;
+import com.retail.service.CategoryService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class CategoryController {
+
+    private final CategoryService categoryService;
     
+    // adding a new category 
     @PostMapping("categories")
+    @ResponseStatus(HttpStatus.CREATED)
     public CategoryResponse addCategory(@RequestBody CategoryRequest request){
-        return null;
+        return categoryService.addCategory(request);
     }
 }
--- retail/src/main/java/com/retail/service/CategoryService.java
+++ retail/src/main/java/com/retail/service/CategoryService.java
@@ -1,8 +1,11 @@
 package com.retail.service;
 
+import org.springframework.stereotype.Service;
+
 import com.retail.io.CategoryRequest;
 import com.retail.io.CategoryResponse;
 
+@Service
 public interface CategoryService {
     
     CategoryResponse addCategory(CategoryRequest request);
