e1169b049b75126355c43d7f5193c19275b99de5
wastedx7 <wastedx7@gmail.com>
2026-04-21 19:14:43 +0530

    serviceimp

--- retail/src/main/java/com/retail/service/CategoryServiceImp.java
+++ retail/src/main/java/com/retail/service/CategoryServiceImp.java
@@ -1,12 +1,46 @@
 package com.retail.service;
 
+import java.util.UUID;
+
+import com.retail.entities.CategoryEntity;
 import com.retail.io.CategoryRequest;
 import com.retail.io.CategoryResponse;
+import com.retail.repository.CategoryRepository;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class CategoryServiceImp implements CategoryService{
+
+    private final CategoryRepository categoryRepository;
 
     @Override
     public CategoryResponse addCategory(CategoryRequest request){
-        return null;
+        CategoryEntity newCategory = convertToEntity(request);
+        newCategory = categoryRepository.save(newCategory);
+        return convertToResponse(newCategory);  
+    }
+
+
+    // both are private methods
+    private CategoryResponse convertToResponse(CategoryEntity newCategory) {
+        return CategoryResponse.builder()
+            .categoryId(newCategory.getCategoryId())
+            .name(newCategory.getName())
+            .description(newCategory.getName())
+            .bgColor(newCategory.getBgColor())
+            .imgUrl(newCategory.getImgUrl())
+            .createdAt(newCategory.getCreatedAt())
+            .updatedAt(newCategory.getUpdatedAt())
+            .build();
+    }
+
+    private CategoryEntity convertToEntity(CategoryRequest request) {
+        return CategoryEntity.builder()
+            .categoryId(UUID.randomUUID().toString())
+            .name(request.getName())
+            .description(request.getDescription())
+            .bgColor(request.getBgColor())
+            .build();
     }
 }
