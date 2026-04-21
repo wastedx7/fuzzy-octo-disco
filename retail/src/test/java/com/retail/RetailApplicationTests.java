848b628a8ebb9cf17459f503c63217640a8c0acb
wastedx7 <wastedx7@gmail.com>
2026-04-21 18:27:14 +0530

    init

---
+++ retail/src/test/java/com/retail/RetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
